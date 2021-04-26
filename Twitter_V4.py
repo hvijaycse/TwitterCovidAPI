30abe047aa9cda83300edb914a24b01bcdee6f9c
Harsh Vijay <hvijay.cse@gmail.com>
2021-04-26 19:47:32 +0530

    enviroment variable

--- Twitter_V4.py
+++ Twitter_V4.py
@@ -5,10 +5,10 @@ import os
 
 
 ts = TwitterSearch(
-    consumer_key=S3Connection(os.environ['S3_KEY'], os.environ['consumer_key']),
-    consumer_secret=S3Connection(os.environ['S3_KEY'], os.environ['consumer_secret']),
-    access_token=S3Connection(os.environ['S3_KEY'], os.environ['access_token']),
-    access_token_secret=S3Connection(os.environ['S3_KEY'], os.environ['access_token_secret']),
+    consumer_key=os.environ.get('consumer_key', None),
+    consumer_secret=os.environ.get('consumer_secret', None),
+    access_token=os.environ.get('access_token', None),
+    access_token_secret=os.environ.get('access_token_secret', None),
 )
 
 
